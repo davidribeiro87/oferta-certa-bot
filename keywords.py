88bb34ce0365e4e6b325cafab9cdf8be6fce6463
davidribeiro87 <davidribeiro87@gmail.com>
2025-08-09 22:20:23 -0300

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,7 @@
+KEYWORDS = [
+    "smartphone", "Android", "iPhone", "notebook", "laptop", "SSD", "monitor",
+    "headphone", "fone bluetooth", "smartwatch", "TV 4K", "Fire TV", "Echo",
+    "roteador", "impressora", "gamer", "placa de vídeo", "teclado mecânico",
+    "air fryer", "cafeteira", "liquidificador", "aspirador", "robô aspirador",
+    "panelas", "frigideira", "purificador de água", "ventilador", "luminária"
+]
